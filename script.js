d2d00ff362a6479b1972d3302d0e9aca8750ac19
Ana Blasco <ana.blasco@espossible.com>
2025-02-18 14:10:22 +0100

    click area fix

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ document.querySelector(".container").addEventListener("click", function(event) {
     let y = event.clientY - rect.top;  
 
     // Click area
-    let regionX1 = 1341, regionX2 = 1624; 
-    let regionY1 = 377, regionY2 = 587; 
+    let regionX1 = 1080, regionX2 = 1200; 
+    let regionY1 = 255, regionY2 = 295; 
 
     if (x >= regionX1 && x <= regionX2 && y >= regionY1 && y <= regionY2) {
         let audio = document.getElementById("audio");
