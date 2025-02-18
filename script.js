fd310bdbd8e4a65ce697861eb4c5f7369baeca06
Ana Blasco <ana.blasco@espossible.com>
2025-02-18 13:30:49 +0100

    progress audio

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Página cargada!");
+document.querySelector(".container").addEventListener("click", function(event) {
+    console.log("click detectado en el container")
+    let container = event.currentTarget;
+    let rect = container.getBoundingClientRect(); 
+
+    let x = event.clientX - rect.left; 
+    let y = event.clientY - rect.top;  
+
+    // Click area
+    let regionX1 = 1341, regionX2 = 1624; 
+    let regionY1 = 377, regionY2 = 587; 
+
+    if (x >= regionX1 && x <= regionX2 && y >= regionY1 && y <= regionY2) {
+        let audio = document.getElementById("audio");
+        audio.play();
+    }
 });
